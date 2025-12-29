b9550ae838163d11eefdf6b14461dedd0b6490b8
zafher-byte <zafher@gmail.com>
2025-12-29 12:17:40 +0300

    Delete file_rename.py

--- file_rename.py
+++
@@ -1,7 +0,0 @@
-"""
-file_rename.py
-
-This script renames all files in a given folder using a simple numbering format.
-It keeps the original file extensions and applies a clean, consistent file name.
-The script is created for basic automation practice.
-"""
